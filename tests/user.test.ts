c47eb5ebc5a859981a49fefeb412cfc93c49f2e0
Leviinson <103764172+Leviinson@users.noreply.github.com>
2025-04-13 19:04:41 +0200

    test: connecting and closing of the db cursor added, new tests added
    test: connecting and closing of the db cursor added, new tests added
    
    1. jest was closing db connection improperly without  closeDb util, now it is fixed
    2. test for checking if user receives 401 error on try to access user profile without access token in "Authorization" was provided
    3. test for checking if authorized user receives information about userprofile was added

--- tests/user.test.ts
+++ tests/user.test.ts
@@ -1,5 +1,29 @@
-describe("User", () => {
-    it("should pass dummy test", () => {
-        expect(true).toBe(true);
+import request from "supertest";
+
+import app from "../src/app";
+import { connectToDb, closeDb } from "../src/db/cursor";
+
+beforeAll(async () => {
+    await connectToDb();
+});
+
+afterAll(async () => {
+    await closeDb();
+});
+
+describe("GET /", () => {
+    it("(unauthorized): it must redirect to the /auth/sigin", async () => {
+        const res = await request(app).get("/");
+        console.log(res.text);
+        expect(res.status).toBe(401);
+    });
+
+    it("(authorized): it must display user profile", async () => {
+        const res = await request(app).get("/");
+        expect(res.body).toEqual({
+            id: 1,
+            username: "levinsxn",
+            email: "example@domain.com",
+        });
     });
 });

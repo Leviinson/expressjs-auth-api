4108903d98f43ea1c42d792c37768cce897399a9
Leviinson <103764172+Leviinson@users.noreply.github.com>
2025-04-14 23:39:29 +0200

    feat(routers): middlewares connecting was added

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,8 +1,13 @@
 import { Router } from "express";
 
 import { userProfileController } from "../controllers/user";
+import loadUser from "../middlewares/loadUser";
+import requireAuth from "../middlewares/requireAuth";
 
 const userRouter = Router();
+
+userRouter.use(requireAuth);
+userRouter.use(loadUser);
 userRouter.get("/", userProfileController);
 
 export default userRouter;

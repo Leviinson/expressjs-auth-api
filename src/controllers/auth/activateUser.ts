b9e3dd15aaf66524585062e8fd66b3bb4f80a1de
Leviinson <103764172+Leviinson@users.noreply.github.com>
2025-04-15 20:11:16 +0200

    refactor: change return type of controller activateUserController

--- src/controllers/auth/activateUser.ts
+++ src/controllers/auth/activateUser.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
 
-function activateUserController(_req: Request, _res: Response): undefined {}
+function activateUserController(_req: Request, _res: Response): void {}
 
 export default activateUserController;

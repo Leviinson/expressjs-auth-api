1ad551fde8f4790da9ac31d9eace241221ff8424
Leviinson <103764172+Leviinson@users.noreply.github.com>
2025-04-15 13:48:50 +0200

    feat(routers): controllers were added to auth router
    feat(routers): controllers were added to auth router
    
    Controllers for signing up, account activation and jwt access token refreshing were connected

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,8 +1,15 @@
 import { Router } from "express";
 
+import activateUserController from "../controllers/auth/activateUser";
+import refreshJWToken from "../controllers/auth/refreshToken";
 import signInController from "../controllers/auth/signIn";
+import signUpController from "../controllers/auth/signUp";
+
 const authRouter = Router();
 
-authRouter.get("/signin", signInController);
+authRouter.post("/signin", signInController);
+authRouter.post("/signup", signUpController);
+authRouter.post("/activate", activateUserController);
+authRouter.post("/refreshToken", refreshJWToken);
 
 export default authRouter;

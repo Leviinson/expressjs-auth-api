1a012e10747dfe16cc71a7cbaeedc87c3fb22455
Leviinson <103764172+Leviinson@users.noreply.github.com>
2025-04-15 16:50:56 +0200

    feat(serializers): 'serializeUserProfile' serializer created

---
+++ src/controllers/user/serializers/serializeUserProfile.ts
@@ -0,0 +1,13 @@
+import User from "@/src/db/models/User";
+
+interface userProfileSerialized {
+    username: string;
+    email: string;
+}
+
+function serializeUserProfile(user: User): userProfileSerialized {
+    return { username: user.username, email: user.email };
+}
+
+export { userProfileSerialized };
+export default serializeUserProfile;

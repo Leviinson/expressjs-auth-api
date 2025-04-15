0e505ee62f033c13bfe151601975e30495781665
Leviinson <103764172+Leviinson@users.noreply.github.com>
2025-04-15 13:46:32 +0200

    fix(utils): null value assigned to 'userId' response locale if token isn't valid or isn't passed

--- src/middlewares/services/tokenValidation.ts
+++ src/middlewares/services/tokenValidation.ts
@@ -7,6 +7,22 @@ interface Payload extends JwtPayload {
     id: number;
 }
 
+/**
+ * This util validates passed accessToken
+ * decoding JWT token using SECRET_KEY
+ * envvar.
+ *
+ * Also it assignes userId from decoded payload
+ * to the reponse locals.
+ *
+ * It returns boolean value that indicates
+ * if accessToken was passed into "Authorization" header
+ * with "Bearer" authorization scheme prefix and is valid.
+ *
+ * @param headers - The incoming HTTP headers.
+ * @param res - the HttpResponse object.
+ * @returns A boolean indicating if the token was passed and is valid.
+ */
 async function isTokenValid(
     headers: IncomingHttpHeaders,
     res: Response
@@ -23,10 +39,12 @@ async function isTokenValid(
             res.locals.userId = payload.id;
             return true;
         } catch (err) {
-            console.log(err)
+            res.locals.userId = null;
+            console.log(err);
             return false;
         }
     }
+    res.locals.userId = null;
     return false;
 }
 

4c5150f9326bb183a6a3717ac049f57112a3ea3d
Leviinson <103764172+Leviinson@users.noreply.github.com>
2025-04-15 16:51:57 +0200

    test(userprofile): testing of returned user profile data implemented

--- tests/user.test.ts
+++ tests/user.test.ts
@@ -1,20 +1,36 @@
 import request from "supertest";
 
 import app from "../src/app";
+import UserRepo from "../src/db/models/repos/user";
 
 describe("GET /", () => {
     it("(unauthorized): it must redirect to the /auth/sigin", async () => {
         const res = await request(app).get("/");
-        console.log(res.text);
         expect(res.status).toBe(401);
     });
 
-    it("(authorized): it must display user profile", async () => {
-        const res = await request(app).get("/");
-        expect(res.body).toEqual({
-            id: 1,
+    it("(authorized, is not activated): it must display user profile", async () => {
+        const userData = {
+            email: "test@gmail.com",
             username: "levinsxn",
-            email: "example@domain.com",
+            password: "dsaasddsa",
+        };
+        const user = await new UserRepo().create({ ...userData });
+
+        const signInRes = await request(app)
+            .post("/auth/signin")
+            .send({ username: userData.username, password: userData.password });
+        const accessToken = signInRes.body.access;
+
+        const userProfileRes = await request(app)
+            .get("/")
+            .set("Authorization", `Bearer ${accessToken}`);
+        expect(userProfileRes.status).toStrictEqual(200);
+        expect(userProfileRes.body).toEqual({
+            username: user.username,
+            email: user.email,
         });
+
+        await user.destroy();
     });
 });

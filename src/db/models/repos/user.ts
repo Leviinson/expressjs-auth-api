861530cf3e68d0701c96292fda91fec75e72c494
Leviinson <103764172+Leviinson@users.noreply.github.com>
2025-04-15 13:44:25 +0200

    feat(repo): 'create' method added to UserRepo

--- src/db/models/repos/user.ts
+++ src/db/models/repos/user.ts
@@ -1,14 +1,24 @@
-import User from "../User";
+import hashPassword from "../../../services/auth/hashers";
+import User, { UserCreationAttributes } from "../User";
+
+// type UpdateUserPasswordById = { id: number; user?: never };
+// type UpdateUserPasswordByUser = { user: User; id?: never };
+// type UpdateUserPassword = (
+//     | UpdateUserPasswordById
+//     | UpdateUserPasswordByUser
+// ) & { password: string };
 
 class UserRepo {
     async getUserById(id: number): Promise<User | null> {
-        try {
-            return await User.findOne({ where: { id } });
-        } catch (err) {
-            console.error(err);
-            return null;
-        }
+        return await User.findOne({ where: { id } });
     }
+
+    async create(userParams: UserCreationAttributes): Promise<User> {
+        userParams.password = hashPassword(userParams.password);
+        return await User.create({ ...userParams });
+    }
+
+    // async updatePassword(params: UpdateUserPassword) {}
 }
 
 export default UserRepo;

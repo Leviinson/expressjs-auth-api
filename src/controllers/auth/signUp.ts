f08fba95e803391569be1ca3994493ea4080787b
Leviinson <103764172+Leviinson@users.noreply.github.com>
2025-04-18 13:34:21 +0200

    feat(controllers): signUpController created

--- src/controllers/auth/signUp.ts
+++ src/controllers/auth/signUp.ts
@@ -1,5 +1,25 @@
 import { Request, Response } from "express";
+import { validationResult } from "express-validator";
 
-function signUpController(_req: Request, _res: Response): void {}
+import UserRepo from "@/db/models/repos/user";
+
+import sendConfirmationEmail from "./services/mailer";
+
+async function signUpController(req: Request, res: Response): Promise<void> {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        res.status(400).json({ errors: errors.array() });
+    } else {
+        const userRepo = new UserRepo();
+        const { username, email, password } = req.body;
+        await userRepo.create({
+            username: username,
+            email: email,
+            password: password,
+        });
+        await sendConfirmationEmail(email, username);
+        res.status(201).json({ message: "Please, confirm you email." });
+    }
+}
 
 export default signUpController;

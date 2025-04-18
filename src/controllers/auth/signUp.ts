019e628407fdb04af1b6fbdcb85e0545ad3da6e5
Leviinson <103764172+Leviinson@users.noreply.github.com>
2025-04-18 16:41:47 +0200

    feat(controllers): confirmation token creation, sending and user creation included into one atomic transaction

--- src/controllers/auth/signUp.ts
+++ src/controllers/auth/signUp.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { validationResult } from "express-validator";
 import { v4 as uuidv4 } from "uuid";
 
+import sequelize from "@/db/models/init";
 import ConfirmationTokenRepo from "@/db/models/repos/ConfirmationToken";
 import UserRepo from "@/db/models/repos/user";
 
@@ -16,24 +17,34 @@ async function signUpController(req: Request, res: Response): Promise<void> {
     } else {
         const { username, email, password } = req.body;
 
-        const userRepo = new UserRepo();
-        const user = await userRepo.create({
-            username: username,
-            email: email,
-            password: password,
-        });
+        const transaction = await sequelize.transaction();
+        try {
+            const userRepo = new UserRepo();
+            const user = await userRepo.create({
+                username: username,
+                email: email,
+                password: password,
+            });
 
-        const confirmationToken = await new ConfirmationTokenRepo().create({
-            expAt: generateExpirationDate(new Date()),
-            userId: user.id,
-            value: uuidv4(),
-        });
-        await sendConfirmationEmail(email, username, confirmationToken.value);
-
-        res.status(201).json({
-            id: user.id,
-            message: "Please, confirm you email.",
-        });
+            const confirmationToken = await new ConfirmationTokenRepo().create({
+                expAt: generateExpirationDate(new Date()),
+                userId: user.id,
+                value: uuidv4(),
+            });
+            await sendConfirmationEmail(
+                email,
+                username,
+                confirmationToken.value
+            );
+            await transaction.commit();
+            res.status(201).json({
+                id: user.id,
+                message: "Please, confirm you email.",
+            });
+        } catch (err) {
+            await transaction.rollback();
+            throw err;
+        }
     }
 }
 

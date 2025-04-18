41fa4a23322c22f95d31927db79e59bb278e12dd
Leviinson <103764172+Leviinson@users.noreply.github.com>
2025-04-18 16:42:00 +0200

    refactor: unused variable removed

--- src/tests/auth/signin.test.ts
+++ src/tests/auth/signin.test.ts
@@ -2,7 +2,7 @@ import createUserAndLogin from "../services/auth";
 
 describe("POST /auth/signin", () => {
     it("must return access and refresh token", async () => {
-        const { response, user } = await createUserAndLogin({ isActive: true });
+        const { response } = await createUserAndLogin({ isActive: true });
         expect(response.status).toStrictEqual(200);
     });
 });

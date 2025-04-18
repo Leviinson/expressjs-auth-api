7c7cc815c93b8608b817188067da4bfb79424d83
Leviinson <103764172+Leviinson@users.noreply.github.com>
2025-04-18 15:36:06 +0200

    feat(controllers): confirmationToken generation and passing to the template implemented

--- src/controllers/auth/signUp.ts
+++ src/controllers/auth/signUp.ts
@@ -1,24 +1,39 @@
 import { Request, Response } from "express";
 import { validationResult } from "express-validator";
+import { v4 as uuidv4 } from "uuid";
 
+import ConfirmationTokenRepo from "@/db/models/repos/ConfirmationToken";
 import UserRepo from "@/db/models/repos/user";
 
 import sendConfirmationEmail from "./services/mailer";
+import { generateExpirationDate } from "./services/signUp";
 
 async function signUpController(req: Request, res: Response): Promise<void> {
     const errors = validationResult(req);
+
     if (!errors.isEmpty()) {
         res.status(400).json({ errors: errors.array() });
     } else {
-        const userRepo = new UserRepo();
         const { username, email, password } = req.body;
-        await userRepo.create({
+
+        const userRepo = new UserRepo();
+        const user = await userRepo.create({
             username: username,
             email: email,
             password: password,
         });
-        await sendConfirmationEmail(email, username);
-        res.status(201).json({ message: "Please, confirm you email." });
+
+        const confirmationToken = await new ConfirmationTokenRepo().create({
+            expAt: generateExpirationDate(new Date()),
+            userId: user.id,
+            value: uuidv4(),
+        });
+        await sendConfirmationEmail(email, username, confirmationToken.value);
+
+        res.status(201).json({
+            id: user.id,
+            message: "Please, confirm you email.",
+        });
     }
 }
 

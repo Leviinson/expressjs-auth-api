ab92b4c38ecf4bf2616507a32970d95625a9e2ed
Leviinson <103764172+Leviinson@users.noreply.github.com>
2025-04-22 01:01:09 +0200

    feat(repos): methods 'getUserByEmail' and 'usernameAndEmailAreFree' added

--- src/db/models/repos/user.ts
+++ src/db/models/repos/user.ts
@@ -1,5 +1,6 @@
 import User, { UserAttributes, UserCreationAttributes } from "@/db/models/User";
 import logger from "@/services/log";
+import { Op } from "sequelize";
 
 // type UpdateUserPasswordById = { id: number; user?: never };
 // type UpdateUserPasswordByUser = { user: User; id?: never };
@@ -27,6 +28,38 @@ class UserRepo {
             where: { username },
             attributes: attributes,
         });
+    }
+    async getUserByEmail(
+        email: string,
+        attributes?: (keyof UserAttributes)[]
+    ): Promise<User | null> {
+        return await User.findOne({
+            where: { email },
+            attributes: attributes,
+        });
+    }
+
+    async usernameAndEmailAreFree(
+        username: string,
+        email: string
+    ): Promise<{ isFree: boolean; takenFields: string[] }> {
+        const user = await User.findOne({
+            where: {
+                [Op.or]: [{ email: email }, { username: username }],
+            },
+            attributes: ["username", "email"],
+        });
+
+        if (!user) {
+            return { isFree: true, takenFields: [] };
+        }
+
+        const takenFields: string[] = [];
+
+        if (user.email == email) takenFields.push("email");
+        if (user.username == username) takenFields.push("username");
+
+        return { isFree: false, takenFields: takenFields };
     }
 
     async create(userParams: UserCreationAttributes): Promise<User> {

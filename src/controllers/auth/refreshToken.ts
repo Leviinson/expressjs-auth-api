a207881b301617215941191c2be79ee7b37a2397
Leviinson <103764172+Leviinson@users.noreply.github.com>
2025-04-22 00:59:55 +0200

    feat(jwt): expiration date and sameSite specified

--- src/controllers/auth/refreshToken.ts
+++ src/controllers/auth/refreshToken.ts
@@ -19,6 +19,8 @@ async function refreshJWToken(req: Request, res: Response): Promise<void> {
         httpOnly: true,
         signed: true,
         secure: process.env.NODE_ENV === "production",
+        expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+        sameSite: "strict",
     });
     res.status(200).json({ access: access });
 }

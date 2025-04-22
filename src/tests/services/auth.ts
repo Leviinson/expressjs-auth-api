9b8724fd044dd570ed00c7e6d2b37af07400f4f7
Leviinson <103764172+Leviinson@users.noreply.github.com>
2025-04-22 16:08:19 +0200

    fix!(tests): csrfToken initializing before every POST request implemented

--- src/tests/services/auth.ts
+++ src/tests/services/auth.ts
@@ -1,8 +1,22 @@
+import { Request } from "express";
 import request, { Test } from "supertest";
 import TestAgent from "supertest/lib/agent";
 
 import app from "@/app";
 import UserRepo from "@/db/models/repos/user";
+
+function extractCSRFTokenFromHeaders(headers: Request["headers"]) {
+    const setCookiesHeaders = headers["set-cookie"];
+    const csrfTokenUnparsed = setCookiesHeaders!.find((cookie) =>
+        cookie.startsWith("csrftoken=")
+    );
+    return csrfTokenUnparsed?.split(";")[0].split("=")[1];
+}
+
+async function getCSRFToken(requestAgent: TestAgent<Test>): Promise<string> {
+    const responseWithCSRFToken = await requestAgent.get("/me");
+    return extractCSRFTokenFromHeaders(responseWithCSRFToken.headers)!;
+}
 
 async function createUserAndLogin(
     userDataOverride = {},
@@ -19,10 +33,19 @@ async function createUserAndLogin(
     const user = await userRepo.create({ ...userData });
 
     const requestAgent = agent ? agent : request(app);
+    const csrfToken = await getCSRFToken(requestAgent);
+    expect(csrfToken).toBeDefined();
+
     const signInRes = await requestAgent
         .post("/auth/signin")
-        .send({ username: userData.username, password: userData.password });
-    return { response: signInRes, user: user };
+        .set("Cookie", `csrftoken=${csrfToken}`)
+        .send({
+            username: userData.username,
+            password: userData.password,
+            csrftoken: csrfToken,
+        });
+    return { response: signInRes, user: user, csrfToken: csrfToken };
 }
 
+export { getCSRFToken };
 export default createUserAndLogin;
